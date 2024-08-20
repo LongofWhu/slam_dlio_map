4184d8032fe73067721c301c5438201732bcbcba
long <1272772507@qq.com>
2024-08-20 09:53:16 +0800

    dlio_v1.1.1

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/long/Project/dlio/src
+# Build directory: /home/long/Project/dlio/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/long/Project/dlio/devel:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/noetic/bin:/home/long/.local/bin:/home/long/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PWD='/home/long/Project/dlio/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/long/Project/dlio/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/long/Project/dlio/src:$ROS_PACKAGE_PATH"
